bef553946d8ad4a1076d4d19dbf6e5de133a9b4e
katerina-shche <asti.katerina@gmail.com>
2024-01-30 11:03:07 +0100

    Navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/auth/Login.vue'
+import Signup from '../views/auth/Signup.vue'
+import CreatePlaylist from '../views/playlists/CreatePlaylist.vue'
 
 const routes = [
   {
@@ -12,6 +14,15 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  { path: '/playlists/create',
+    name: 'CreatePlaylist',
+    component: CreatePlaylist
   }
 ]
 
