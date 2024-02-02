12642529f681f7056d0b51ca63cd6223c23608c5
katerina-shche <asti.katerina@gmail.com>
2024-02-02 21:11:13 +0100

    ListVeiw setup

---
+++ src/components/ListView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-for="playlist in playlists" :key="playlist.id">
+        <div class="single">
+            <div class="thumbnail">
+                <img :src="playlist.coverUrl" alt="cover">
+            </div>
+            <div class="info">
+                <h3>{{ playlist.title }}</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['playlists']
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="home">
    <p>Homepage</p>
+   <div v-if="error" class="error"> Could not fetch the data</div>
+   <div v-else>
+      <ListView :playlists="documents" />
+   </div>
     
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { getCollection } from '@/composables/getCollection'
+import { projectFirestore } from '@/firebase/config'
+import ListView from '@/components/ListView.vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: { ListView },
+  setup() {
+    const { documents, error } = getCollection('playlists')
+
+    return { documents, error }
+  }
 }
 </script>
