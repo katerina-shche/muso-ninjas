0fdd824706bca07a40dd8a042fe52352a8e09841
katerina-shche <asti.katerina@gmail.com>
2024-02-06 13:18:12 +0100

    getDocument

---
+++ src/composables/getDocument.js
@@ -0,0 +1,47 @@
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from "@/firebase/config"
+import {
+    //getFirestore, 
+    collection, 
+    //getDocs,
+    onSnapshot,
+    addDoc, 
+    //deleteDoc, doc,
+    query,
+    //where,
+    orderBy,
+    //serverTimestamp,
+    //getDoc, updateDoc
+} from 'firebase/firestore'
+
+const getDocument = (collectionName, id) => {
+    const document = ref(null)
+    const error = ref(null)
+
+    let colRef = collection(projectFirestore, collectionName)
+    let docRef = doc(projectFirestore, colRef, id)
+    const unsubDoc = onSnapshot(docRef, (doc) => {
+        if(doc.data()) {
+        document.value = { ...doc.data(), id: doc.id}
+        error.value = null
+        console.log(document.value)
+        } else {
+            error.value = 'that document does not exist'
+        }
+        
+}, (err) => {
+    console.log(err.message)
+    error.value = 'could not fetch data'
+})
+
+watchEffect((onInvalidate) => {
+    //unsub from onSnapshot document when watcher is stopped (component unmounted)
+    onInvalidate(() => {
+        unsubDoc()
+    })
+})
+
+return { document, error }
+}
+
+export { getDocument }
