d52d45efc24959ec5ede03d91aaa0bd5265bfb1b
katerina-shche <asti.katerina@gmail.com>
2024-02-08 14:31:26 +0100

    listing songs

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -15,7 +15,14 @@
             </div>
             <!-- song list -->
             <div class="song-list">
-                <p>song list here</p>
+                <div v-if="!playlist.songs.length">No songs have been added to this playlist yet</div>
+                <div v-else v-for="song in playlist.songs" :key="song.id" class="single-song">
+                    <div class="details">
+                        <h3>{{ song.title }}</h3>
+                        <p>{{ song.artist }}</p>
+                    </div>
+                    <button v-if="ownership">Delete</button>
+                </div>
                 <AddSong v-if="ownership" :playlist="playlist" />
             </div>  
         </div>
@@ -92,5 +99,13 @@ import { useRouter } from 'vue-router'
 }
 .description {
     text-align: left;
+}
+.single-song {
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px dashed var(--secondary);
+    margin-bottom: 20px;
 }
 </style>
